eaa200e6290dbc62494de79170230b69695bb203
MinhQuang1711 <phamminhquang.work@gmail.com>
2023-09-08 15:13:03 +0700

    Create domain models

---
+++ Data/Domains/DetailProduct.cs
@@ -0,0 +1,14 @@
+﻿namespace Ecommerce_BE.Data.Domains
+{
+    public class DetailProduct: BaseModel
+    {
+
+        public double weight { get; set; }
+        public double sumCost { get; set; }
+        public string productID { get; set; }
+        public string ingerdientID { get; set; }
+
+        DetailProduct() { }
+
+    }
+}
